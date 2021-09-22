4bcfa6854b270916dd9075126a87368dd887ca90
SoftAcademy/Plovdiv University/Georgi Angelov <60781748+georgiangelov2000@users.noreply.github.com>
2021-09-22 19:31:19 +0300

    sessions

---
+++ 10.Sessions/1.sessions.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>Hello World</h1>
+
+
+<?php
+
+
+
+
+
+// session_start();
+// if ( ! isset($_SESSION['count'])) {
+//    echo $_SESSION['count'] = 0;
+// } else {
+//   echo  $_SESSION['count']++;
+// }
+
+    // if(isset($_COOKIE['user'])){
+    //     echo 'welcome' .$_COOKIE['user'].'!<br>';
+    // }else{
+    //     echo "Welcome guest! <br>";
+    //     setcookie('user','georgi',time() +60);
+    // }
+?>
+
+<!-- 
+<script type="text/javascript"> 
+
+// save data to session storage
+sessionStorage.setItem('key','value');
+
+// get saved data from sessionStorage
+let data= sessionStorage.getItem('key');
+
+// save data to localStorage
+localStorage.setItem('favoriteFlavor','vanilla');
+
+// get saved data from localstorage
+let taste=localStorage.getItem('favoriteFlavor')
+</script> -->
+    
+</body>
+</html>

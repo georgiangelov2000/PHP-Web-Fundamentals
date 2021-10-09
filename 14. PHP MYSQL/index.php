903cd93c351456102dbf484a63f4e12dedd1a4fd
SoftAcademy/Plovdiv University/Georgi Angelov <60781748+georgiangelov2000@users.noreply.github.com>
2021-10-09 19:51:41 +0300

    setup register and login

---
+++ 14. PHP MYSQL/index.php
@@ -0,0 +1,16 @@
+<form method="get" action="index.php">
+    <input type="text" name="first_name" />
+    <input type="submit" value="Търси!" name="search"/>
+</form>
+
+<?php 
+    if(isset($_GET['search'])){
+        $firstName = $_GET['first_name'];
+        $db = new PDO("mysql:host=localhost;dbname=sex;charset=utf8", "root", "");
+        $stmt=$db->prepare('SELECT * from people WHERE first_name=?');
+        $argumentsToDatabase = [$firstName];
+        $stmt->execute($argumentsToDatabase);
+        $person = $stmt->fetch(PDO::FETCH_ASSOC);
+        var_dump($person); 
+    }
+?>
